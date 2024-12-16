e2996bba6c3148edecea1a04303529e5f57534cf
Aleksandr Sokolov <killycite@gmail.com>
2024-12-16 17:43:11 +0300

    Внедрение функции публикации поста

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -16,8 +20,46 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", nil)
 }
 
+func publication(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/publication.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "publication", nil)
+}
+
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	//проверки
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("Insert into `articles` (`title`, `anons`, `full_text`) Values('%s', '%s', '%s')", title, anons, full_text))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+	}
+}
+
 func handleFunc() {
 	http.HandleFunc("/home", index)
+	http.HandleFunc("/publication", publication)
+	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":8080", nil)
 }
 
