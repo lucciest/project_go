38e59cc6138e515a3770c33a4f04ed47b0ea7083
Aleksandr Sokolov <killycite@gmail.com>
2024-12-16 20:16:46 +0300

    Отображение постов на главной старнице

--- main.go
+++ main.go
@@ -1,30 +1,64 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
 
-	"database/sql"
-
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
+
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+var showPost = Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	//Выборка данных
+	res, err := db.Query("Select *  from `articles`")
+	if err != nil {
+		panic(err)
+	}
+
+	posts = []Article{}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func publication(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/publication.html", "templates/header.html", "templates/footer.html")
 
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		http.Error(w, "Ошибка рендеринга шаблона", http.StatusInternalServerError)
 	}
 
 	t.ExecuteTemplate(w, "publication", nil)
@@ -56,10 +90,51 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func show_post(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
+	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	//Выборка данных
+	res, err := db.Query(fmt.Sprintf("Select *  from `articles` Where `id` ='%s'", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+
+	showPost = Article{}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+
+		showPost = post
+	}
+	t.ExecuteTemplate(w, "show", showPost)
+}
+
 func handleFunc() {
-	http.HandleFunc("/home", index)
-	http.HandleFunc("/publication", publication)
-	http.HandleFunc("/save_article", save_article)
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/home", index).Methods("GET")
+	rtr.HandleFunc("/publication", publication).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+
+	http.Handle("/", rtr)
 	http.ListenAndServe(":8080", nil)
 }
 
